5058609016991d622b6e44f2e7cb2d19491c5509
Atul Sureka <ASureka@slb.com>
2021-07-10 17:07:10 +0530

    Another commit

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-//console.log("I am called.");
+console.log("I am called.");
 
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
 
-app.listen(port, () => {
-  console.log("Example app listening at http://localhost:${port}");
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// })
+
+// app.listen(port, () => {
+//   console.log("Example app listening at http://localhost:"+port);
+// })
